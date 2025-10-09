8746594b0d6af78be92c5d3776408ccba61e18d0
NelakaWith <nelaka.with@gmail.com>
2025-10-09 11:44:18 +0530

    feat(components): update Button component styles for improved consistency and readability
    feat(tailwind): extend color palette with new washed and primary color variants

--- src/components/common/button.jsx
+++ src/components/common/button.jsx
@@ -1,7 +1,7 @@
 function Button() {
   return (
     <button
-      className="inline-flex h-8 items-center gap-[7px] px-2 py-1 relative rounded-lg border-[none] bg-[linear-gradient(180deg,rgba(3,0,20,1)_0%,rgba(40,38,55,1)_100%)] before:content-[''] before:absolute before:inset-0 before:p-px before:rounded-lg before:[background:linear-gradient(90deg,rgba(32,31,48,1)_0%,rgba(70,69,83,1)_100%)] before:[-webkit-mask:linear-gradient(#fff_0_0)_content-box,linear-gradient(#fff_0_0)] before:[-webkit-mask-composite:xor] before:[mask-composite:exclude] before:z-[1] before:pointer-events-none cursor-pointer transition-opacity hover:opacity-90 active:opacity-80"
+      className=" inline-flex h-8 items-center gap-[7px] px-2 py-1 relative rounded-lg border-[none] bg-[linear-gradient(180deg,rgba(3,0,20,1)_0%,rgba(40,38,55,1)_100%)] before:content-[''] before:absolute before:inset-0 before:p-px before:rounded-lg before:[background:linear-gradient(90deg,rgba(32,31,48,1)_0%,rgba(70,69,83,1)_100%)] before:[-webkit-mask:linear-gradient(#fff_0_0)_content-box,linear-gradient(#fff_0_0)] before:[-webkit-mask-composite:xor] before:[mask-composite:exclude] before:z-[1] before:pointer-events-none cursor-pointer transition-opacity hover:opacity-90 active:opacity-80"
       type="button"
       aria-label="Button CTA"
     >
